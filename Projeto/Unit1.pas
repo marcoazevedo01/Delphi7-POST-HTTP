eabf702af1426706ca2d5e8502dc30ba1ee2ce2d
Marco A S Azevedo <37423042+marcoazevedo01@users.noreply.github.com>
2021-03-12 02:11:09 -0300

    v0.0.1
    commit inicial

---
+++ Projeto/Unit1.pas
@@ -0,0 +1,58 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, IdBaseComponent, IdComponent, IdTCPConnection,
+  IdTCPClient, IdHTTP, IdIOHandler, IdIOHandlerSocket, IdSSLOpenSSL;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    IdHTTP1: TIdHTTP;
+    procedure Button1Click(Sender: TObject);
+
+  private
+    { Private declarations }
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+ var
+  s: String;
+  Resp_Json: string;
+  Req_Json:TStream;
+
+begin
+
+    try
+        s:='cod=codDoProduto';
+        s:=s+'&price=PrecoDoproduto';
+        s:=s+'&token=tokenDoMercadoLimit255';
+        Req_Json:=TstringStream.Create(s);
+        Req_Json.Position:=0;
+        IdHTTP1.Request.UserAgent:='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
+        IdHTTP1.Request.Accept := 'application/json, text/javascript, */*; q=0.01';
+        IdHTTP1.Request.ContentType := 'application/x-www-form-urlencoded; charset=UTF-8';
+        Resp_Json:=IdHTTP1.Post('http://localhost:3000/integration/api', Req_Json);
+    finally
+        Req_Json.Free;
+    end;
+           //continua o seu fluxo normal independete.
+           //DMconecao.tabela.post;
+           //se quiser verificar o sevrer devolve um json com {msg:'success'} ou error  de qualqer maneira para n parar o fluxo principal o status da reposta sera 202
+end;
+
+end.
+
+
+
